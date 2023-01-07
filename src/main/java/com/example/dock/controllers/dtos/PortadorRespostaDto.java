c4a80893cb59886403d5caf99ce2b6a8c6e4fcf1
wallysonruan <wallysonruan@gmail.com>
2023-01-07 15:20:03 -0300

    UPDATE: Removi validacoes desnecessarias e inclui UUID

--- src/main/java/com/example/dock/controllers/dtos/PortadorRespostaDto.java
+++ src/main/java/com/example/dock/controllers/dtos/PortadorRespostaDto.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.br.CPF;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Builder
