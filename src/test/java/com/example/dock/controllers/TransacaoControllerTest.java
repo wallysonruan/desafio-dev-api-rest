dd52b27c00b2c7546f4497d727ea69bd03fd6a27
wallysonruan <wallysonruan@gmail.com>
2023-01-25 12:15:19 -0300

    FEAT: Testes do POST da entidade Transacao

--- src/test/java/com/example/dock/controllers/TransacaoControllerTest.java
+++ src/test/java/com/example/dock/controllers/TransacaoControllerTest.java
@@ -7,6 +7,7 @@ import com.example.dock.models.Transacao;
 import com.example.dock.models.TransacaoTipo;
 import com.example.dock.services.impl.TransacaoServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -45,6 +46,7 @@ class TransacaoControllerTest {
             .portador(
                     Portador.builder()
                     .uuid(UUID.randomUUID())
+                            .cpf("18241327005")
                     .build()
             )
             .totalDaTransacao(BigDecimal.valueOf(10.0))
@@ -69,10 +71,11 @@ class TransacaoControllerTest {
         controller = new TransacaoController(service);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
         objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
     }
 
     @Test
-    void novaTransacao__quandoReceberTransacaoComandoCriarDtoValio__deveriaRetornarTransacaoRespostaDtoE200() throws Exception {
+    void novaTransacao__quandoReceberTransacaoComandoCriarDtoValido__deveriaRetornarTransacaoRespostaDtoE200() throws Exception {
         when(service.novaTransacao(any())).thenReturn(TRANSACAO);
         String TRANSACAO_COMANDO_CRIAR_DTO_JSON = objectMapper.writeValueAsString(TRANSACAO_COMANDO_CRIAR_DTO);
 
@@ -87,6 +90,23 @@ class TransacaoControllerTest {
 
         verify(service, times(1)).novaTransacao(any());
         assertNotNull(responseAsObject);
-        assertEquals(TRANSACAO_RESPOSTA_DTO, responseAsObject);
+        assertEquals(TRANSACAO_RESPOSTA_DTO.uuid, responseAsObject.uuid);
+        assertEquals(TRANSACAO_RESPOSTA_DTO.totalDaTransacao, responseAsObject.totalDaTransacao);
+        assertEquals(TRANSACAO_RESPOSTA_DTO.transacaoTipo, responseAsObject.transacaoTipo);
+        assertEquals(TRANSACAO_RESPOSTA_DTO.portador, responseAsObject.portador);
+    }
+
+    @Test
+    void novaTransacao__quandoReceberTransacaoComandoCriarDtoNull__deveriaRetornarErro404() throws Exception {
+        TransacaoComandoCriarDto TRANSACAO_COMANDO_CRIAR_DTO_NULL = TransacaoComandoCriarDto.builder().build();
+        String TRANSACAO_COMANDO_CRIAR_DTO_NULL_AS_JSON = objectMapper.writeValueAsString(TRANSACAO_COMANDO_CRIAR_DTO);
+
+        var response = mockMvc.perform(
+                        post(URL)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("")
+                ).andExpect(status().isBadRequest());
+
+        verify(service, times(0)).novaTransacao(any());
     }
 }
