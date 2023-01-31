df6488ed9e4042131145026bad732c470a64192c
wallysonruan <wallysonruan@gmail.com>
2023-01-31 20:32:28 -0300

    REFACTOR: Explicitando o nao mapeamento de determinadas propriedades

--- src/main/java/com/example/dock/controllers/AgenciaMapper.java
+++ src/main/java/com/example/dock/controllers/AgenciaMapper.java
@@ -4,10 +4,12 @@ import com.example.dock.controllers.dtos.AgenciaComandoCriarDto;
 import com.example.dock.controllers.dtos.AgenciaRespostaDto;
 import com.example.dock.models.Agencia;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 
 @Mapper(componentModel = "spring")
 public interface AgenciaMapper {
+    @Mapping(target = "id", ignore = true)
     Agencia agenciaComandoCriarDtoToAgencia(AgenciaComandoCriarDto agenciaComandoCriarDto);
 
     AgenciaRespostaDto agenciaToAgenciaRespostaDto(Agencia agencia);
--- src/main/java/com/example/dock/controllers/PortadorMapper.java
+++ src/main/java/com/example/dock/controllers/PortadorMapper.java
@@ -4,9 +4,11 @@ import com.example.dock.controllers.dtos.PortadorComandoCriarDto;
 import com.example.dock.controllers.dtos.PortadorRespostaDto;
 import com.example.dock.models.Portador;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PortadorMapper {
+    @Mapping(target = "uuid", ignore = true)
     Portador portadorComandoCriarDtoToPortador(PortadorComandoCriarDto portadorComandoCriarDto);
 
     PortadorRespostaDto portadorToPortadorRespostaDto(Portador portador);
